cc82c5f8d672436df917ad008ad8d0a566f0e76e
vesder <vsdrrt124@gmail.com>
2025-02-17 01:52:45 +0330

    Add Code

---
+++ src/main/java/me/vesder/blazeydoublejump/BlazeyDoubleJump.java
@@ -0,0 +1,17 @@
+package me.vesder.blazeydoublejump;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BlazeyDoubleJump extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
