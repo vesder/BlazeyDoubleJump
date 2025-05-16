fa497ac0ae5ecba982c5bad3a3c044af354b9101
vesder <vsdrrt124@gmail.com>
2025-05-17 00:00:46 +0330

    Clean Code

---
+++ src/main/java/me/vesder/blazeydoublejump/listeners/InfiniteJump.java
@@ -0,0 +1,35 @@
+package me.vesder.blazeydoublejump.listeners;
+
+import me.vesder.blazeydoublejump.utils.VoidUtils;
+import org.bukkit.GameMode;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerToggleFlightEvent;
+
+import static me.vesder.blazeydoublejump.utils.TextUtils.*;
+
+public class InfiniteJump implements Listener {
+
+    private final double LAUNCH = getDoubleConfig("Settings.Launch_power"),
+            LAUNCHY = getDoubleConfig("Settings.Launch_power_y");
+
+    @EventHandler
+    private void onToggleFlight(PlayerToggleFlightEvent e) {
+
+        Player player = e.getPlayer();
+
+        if (player.getGameMode() == GameMode.CREATIVE ||
+            player.getGameMode() == GameMode.SPECTATOR ||
+            !e.isFlying()) return;
+
+        VoidUtils.sendMsg(player, getStringConfig("Messages.DoubleJump"));
+
+        VoidUtils.playStringSound(player, getStringConfig("Settings.Sound"));
+
+        player.setVelocity(player.getLocation().getDirection().multiply(LAUNCH).setY(LAUNCHY));
+        e.setCancelled(true);
+
+    }
+
+}
