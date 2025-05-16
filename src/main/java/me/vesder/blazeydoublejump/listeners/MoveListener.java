7af88076156626b8158a094278c0f28ce084ef74
vesder <vsdrrt124@gmail.com>
2025-05-17 00:33:12 +0330

    Clean MoveListener Class Code

--- src/main/java/me/vesder/blazeydoublejump/listeners/MoveListener.java
+++ src/main/java/me/vesder/blazeydoublejump/listeners/MoveListener.java
@@ -1,33 +1,24 @@
 package me.vesder.blazeydoublejump.listeners;
 
+import me.vesder.blazeydoublejump.utils.VoidUtils;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
 
-import static me.vesder.blazeydoublejump.listeners.JoinListener.doubleJumpStatus;
-import static me.vesder.blazeydoublejump.utils.TextUtils.color;
 import static me.vesder.blazeydoublejump.utils.TextUtils.getStringConfig;
 
 public class MoveListener implements Listener {
-
-    private final String doubleJumpActive = color(getStringConfig("Messages.DoubleJumpIsActive"));
 
     @EventHandler
     private void onMove(PlayerMoveEvent e) {
 
         Player player = e.getPlayer();
 
-        if (!doubleJumpStatus.get(player.getUniqueId())) {
-            if (player.isOnGround()) {
+        if (player.getAllowFlight() || !player.isOnGround()) return;
 
-                doubleJumpStatus.put(player.getUniqueId(), true);
-                if (!doubleJumpActive.isEmpty()) {
-                    player.sendMessage(doubleJumpActive);
-                }
-
-            }
-        }
+        player.setAllowFlight(true);
+        VoidUtils.sendMsg(player, getStringConfig("Messages.DoubleJumpIsActive"));
 
     }
 
