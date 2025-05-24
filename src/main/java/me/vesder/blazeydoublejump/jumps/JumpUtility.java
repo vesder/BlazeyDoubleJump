dd2050a421030deaefa4104d79c1dc46d0b17ac2
vesder <vsdrrt124@gmail.com>
2025-05-25 02:09:58 +0330

    Rename methods & add javadoc

---
+++ src/main/java/me/vesder/blazeydoublejump/jumps/JumpUtility.java
@@ -0,0 +1,74 @@
+package me.vesder.blazeydoublejump.jumps;
+
+import java.util.HashMap;
+import java.util.UUID;
+
+import static me.vesder.blazeydoublejump.utils.TextUtils.getDoubleConfig;
+
+public class JumpUtility {
+
+    public static final double
+        LAUNCH = getDoubleConfig("Settings.Launch", 10D),
+        LAUNCHY = getDoubleConfig("Settings.LaunchY", 10D),
+        COOLDOWN = getDoubleConfig("Settings.Cooldown") * 1000L;
+
+    private static final HashMap<UUID, Boolean> jumpStatus = new HashMap<>();
+    private static final HashMap<UUID, Long> lastJumpTime = new HashMap<>();
+
+
+    /**
+     * Sets whether the player is currently allowed to jump.
+     *
+     * @param playerId the UUID of the player
+     * @param status   true if the player is allowed to jump; false otherwise
+     */
+    public static void setJumpAllowed(UUID playerId, Boolean status) {
+        jumpStatus.put(playerId, status);
+    }
+
+    /**
+     * Returns whether the player is currently allowed to jump.
+     *
+     * @param playerId the UUID of the player
+     * @return true if the player is allowed to jump; false otherwise
+     */
+    public static boolean isJumpAllowed(UUID playerId) {
+        return jumpStatus.get(playerId);
+    }
+
+    /**
+     * Records the current time as the player's last jump time.
+     *
+     * @param playerId the UUID of the player
+     */
+    public static void setLastJumpTime(UUID playerId) {
+        lastJumpTime.put(playerId, System.currentTimeMillis());
+    }
+
+    /**
+     * Checks if the player is currently on cooldown and cannot jump yet.
+     *
+     * @param playerId the UUID of the player
+     * @return true if the player is on cooldown; false otherwise
+     */
+    public static boolean isJumpOnCooldown(UUID playerId) {
+
+        if (!lastJumpTime.containsKey(playerId)) return false;
+
+        return System.currentTimeMillis() - lastJumpTime.get(playerId) < COOLDOWN;
+    }
+
+    /**
+     * Returns the remaining cooldown time in seconds before the player can jump again.
+     *
+     * @param playerId the UUID of the player
+     * @return remaining cooldown time in seconds, or 0 if no cooldown
+     */
+    public static int getJumpCooldownLeft(UUID playerId) {
+
+        if (!lastJumpTime.containsKey(playerId)) return 0;
+
+        return (int) ((COOLDOWN - (System.currentTimeMillis() - lastJumpTime.get(playerId))) / 1000);
+    }
+
+}
