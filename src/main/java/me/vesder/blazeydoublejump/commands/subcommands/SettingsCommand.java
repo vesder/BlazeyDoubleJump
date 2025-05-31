54edc247aa126d1facdb3d7d883e4ff92ee0c545
vesder <vsdrrt124@gmail.com>
2025-05-31 11:28:16 +0330

    Clean & Optimizing SettingsCommand Code

--- src/main/java/me/vesder/blazeydoublejump/commands/subcommands/SettingsCommand.java
+++ src/main/java/me/vesder/blazeydoublejump/commands/subcommands/SettingsCommand.java
@@ -1,15 +1,17 @@
 package me.vesder.blazeydoublejump.commands.subcommands;
 
 import me.vesder.blazeydoublejump.commands.SubCommand;
-import org.bukkit.Bukkit;
+import me.vesder.blazeydoublejump.utils.TextUtils;
 import org.bukkit.entity.Player;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.logging.Level;
 
 import static me.vesder.blazeydoublejump.commands.CommandManager.getSubCommand;
 import static me.vesder.blazeydoublejump.utils.ConfigUtils.getDoubleConfig;
 import static me.vesder.blazeydoublejump.utils.ConfigUtils.setConfig;
+import static org.bukkit.Bukkit.getLogger;
 
 public class SettingsCommand implements SubCommand {
 
@@ -34,34 +36,19 @@ public class SettingsCommand implements SubCommand {
         if (args.length >= 3) {
             switch (args[1].toLowerCase()) {
                 case "infinitejump":
-                    player.sendMessage(String.format("Setting '%s' successfully updated to '%s'!", args[1], args[2]));
-                    setConfig("Settings.InfiniteJump", Boolean.valueOf(args[2]));
+                    setConfig(args[0] + "." + args[1], Boolean.valueOf(args[2]));
+                    player.sendMessage(TextUtils.color(String.format("&aSetting '%s' successfully updated to '%s'!", args[1], args[2])));
                     return;
                 case "launch":
-                    try {
-                        player.sendMessage(String.format("Setting '%s' successfully updated to '%s'!", args[1], args[2]));
-                        setConfig("Settings.Launch", Double.valueOf(args[2]));
-                        return;
-                    } catch (NumberFormatException ex) {
-                        Bukkit.getLogger().warning(ex.getMessage());
-                        break;
-                    }
                 case "launchy":
-                    try {
-                        player.sendMessage(String.format("Setting '%s' successfully updated to '%s'!", args[1], args[2]));
-                        setConfig("Settings.LaunchY", Double.valueOf(args[2]));
-                        return;
-                    } catch (NumberFormatException ex) {
-                        Bukkit.getLogger().warning(ex.getMessage());
-                        break;
-                    }
                 case "cooldown":
                     try {
-                        player.sendMessage(String.format("Setting '%s' successfully updated to '%s'!", args[1], args[2]));
-                        setConfig("Settings.Cooldown", Double.valueOf(args[2]));
+                        setConfig(args[0] + "." + args[1], Double.valueOf(args[2]));
+                        player.sendMessage(TextUtils.color(String.format("&aSetting '%s' successfully updated to '%s'!", args[1], args[2])));
                         return;
                     } catch (NumberFormatException ex) {
-                        Bukkit.getLogger().warning(ex.getMessage());
+                        player.sendMessage(TextUtils.color("&cInvalid number. Please enter a valid numeric value."));
+                        getLogger().log(Level.WARNING, "Unexpected exception while updating config", ex);
                         break;
                     }
             }
