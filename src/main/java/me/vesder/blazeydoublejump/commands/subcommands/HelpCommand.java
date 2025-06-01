a8f58bed1a64a00abe5df4f7d4dbe15025709824
vesder <vsdrrt124@gmail.com>
2025-06-01 15:57:38 +0330

    Add getHelpMessage method for automated help messages
    Refactor HelpCommand to clean up code and optimize help message generation.

--- src/main/java/me/vesder/blazeydoublejump/commands/subcommands/HelpCommand.java
+++ src/main/java/me/vesder/blazeydoublejump/commands/subcommands/HelpCommand.java
@@ -4,35 +4,64 @@ import me.vesder.blazeydoublejump.commands.CommandManager;
 import me.vesder.blazeydoublejump.commands.SubCommand;
 import org.bukkit.entity.Player;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import static me.vesder.blazeydoublejump.commands.CommandManager.getSubCommand;
+import static me.vesder.blazeydoublejump.commands.CommandManager.getSubCommandNames;
 import static me.vesder.blazeydoublejump.commands.CommandManager.getSubCommands;
 import static me.vesder.blazeydoublejump.utils.TextUtils.color;
 
 public class HelpCommand implements SubCommand {
 
-    private String defaultHelpMessage;
+    private final Map<String, String> cachedHelpMessages = new HashMap<>();
+
+    private static final String HEADER = "&b=====&6===== &d&lBlazeyDoubleJump &6=====&b=====&f\n \n";
+    private static final String FOOTER = "&b=====&6=============================&b=====";
 
     private String getDefaultHelpMessage() {
 
-        if (defaultHelpMessage != null) {
-            return defaultHelpMessage;
+        String commandName = "help";
+
+        if (cachedHelpMessages.containsKey(commandName)) {
+            return cachedHelpMessages.get(commandName);
         }
 
-        StringBuilder message = new StringBuilder();
-        message.append("&b=====&6===== &d&lBlazeyDoubleJump &6=====&b=====")
-            .append("&f\n \n");
+        StringBuilder helpMessageBuilder = new StringBuilder();
+        helpMessageBuilder.append(HEADER);
 
         for (SubCommand subCommand : getSubCommands()) {
-            message.append(subCommand.getSyntax())
+            helpMessageBuilder.append(subCommand.getSyntax())
                 .append("\n")
                 .append(subCommand.getDescription())
                 .append("\n \n");
         }
 
-        message.append("&b=====&6=============================&b=====");
+        helpMessageBuilder.append(FOOTER);
+        cachedHelpMessages.put(commandName, color(helpMessageBuilder.toString()));
 
-        return defaultHelpMessage = color(message.toString());
+        return cachedHelpMessages.get(commandName);
+    }
+
+    private String getHelpMessage(String commandName) {
+
+        if (cachedHelpMessages.containsKey(commandName)) {
+            return cachedHelpMessages.get(commandName);
+        }
+
+        StringBuilder helpMessageBuilder = new StringBuilder();
+        helpMessageBuilder.append(HEADER);
+
+        for (String subArg : getSubCommand(commandName).getSubcommandArguments(null, new String[2])) {
+            helpMessageBuilder.append("/bdj ").append(commandName).append(" ").append(subArg)
+                .append("\n \n");
+        }
+
+        helpMessageBuilder.append(FOOTER);
+        cachedHelpMessages.put(commandName, color(helpMessageBuilder.toString()));
+
+        return cachedHelpMessages.get(commandName);
     }
 
     @Override
@@ -47,14 +76,14 @@ public class HelpCommand implements SubCommand {
 
     @Override
     public String getSyntax() {
-        return "/bdj help";
+        return "/bdj help <command>";
     }
 
     @Override
     public void perform(Player player, String[] args) {
 
-        if (args.length >= 2 && args[1].equalsIgnoreCase("settings")) {
-            player.sendMessage("MOOOW");
+        if (args.length >= 2 && getSubCommandNames().contains(args[1].toLowerCase())) {
+            player.sendMessage(getHelpMessage(args[1].toLowerCase()));
             return;
         }
 
